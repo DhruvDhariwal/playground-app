3278d07246254ea8149b27383a26ef298cd315fc
Dhruv Dhariwal <dhruvdhariwal360@gmail.com>
2025-08-08 22:09:38 +0530

    Disable lightningcss for Vercel build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    optimizeCss: false // disable lightningcss to fix Vercel build
+  }
+}
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+module.exports = nextConfig
